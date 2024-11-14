398a37b2967c3e177092a6e1d739bde5324eb681
Chenle <ycle@live.fr>
2024-11-14 11:07:14 +0100

    3350 in cpp
    the solution uses DP but it is not needed.
    Though the time complexity is decent and the wasted space
    is also accpetable (maybe not, because it's 3n, well not the concern
    here).
    
    But in general the solution is good. Nice

--- 3350_adjacent/solution.cpp
+++ 3350_adjacent/solution.cpp
@@ -9,12 +9,12 @@ public:
         int length = nums.size();
         if (length == 2)
             return 1;
-        // std::get<0>(DP[i]) = previousIncreasingArray
-        // std::get<1>(DP[i]) = currentIncreasingArray
+        // std::get<0>(DP[i]) = previousIncreasingArray.size()
+        // std::get<1>(DP[i]) = currentIncreasingArray.size()
         // std::get<2>(DP[i]) = ret for array from 0 to i
         // end result is std::get<2>(DP[length-1])
-        std::tuple<vector<int>, vector<int>, int> DP[length];
-        DP[0] = {{},{nums[0]},0};
+        std::tuple<int, int, int> DP[length];
+        DP[0] = {0,1,0};
         // for (int i = 1; i < length; ++i) {
         //     std::get<0>(DP[i]) = {};
         //     std::get<1>(DP[i]) = {nums[i-1]};
@@ -22,24 +22,19 @@ public:
         // }
 
         for (int i = 1; i < length; ++i) {
-            if (std::get<0>(DP[i-1]).size())
+            if (std::get<0>(DP[i-1]))
                 std::get<0>(DP[i]) = std::get<0>(DP[i-1]);
             if (nums[i]>nums[i-1]) {
-                std::get<1>(DP[i]) = std::get<1>(DP[i-1]);
-                std::get<1>(DP[i]).push_back(nums[i]);
+                std::get<1>(DP[i]) = std::get<1>(DP[i-1])+1;
             } else {
                 std::get<0>(DP[i]) = std::get<1>(DP[i-1]);
-                // printf("index %d, setting lastArray to size of %zu\n", i, std::get<0>(DP[i]).size());
-                std::get<1>(DP[i]) = {nums[i]};
+                // printf("index %d, setting lastArray to size of %d\n", i, std::get<0>(DP[i]));
+                std::get<1>(DP[i]) = 1;
             }
-            // printf("size of building array is %zu\n", std::get<1>(DP[i]).size());
-            // for (auto aa : std::get<1>(DP[i])) {
-            //     printf("%d ",aa);
-            // }
-            // printf("\n");
-            int m = std::max(std::get<1>(DP[i]).size()/2,
-                             std::min(std::get<1>(DP[i]).size(),
-                                      std::get<0>(DP[i]).size()));
+            // printf("size of building array is %d\n", std::get<1>(DP[i]));
+            int m = std::max(std::get<1>(DP[i])/2,
+                             std::min(std::get<1>(DP[i]),
+                                      std::get<0>(DP[i])));
 
             if (m > std::get<2>(DP[i-1])) {
                 std::get<2>(DP[i]) = m;
@@ -55,7 +50,8 @@ public:
 
 int main() {
     Solution s = Solution();
-    vector<int> input = {2,5,7,8,9,2,3,4,3,1};
-    vector<int> input2 = {1,2,3,4,4,4,4,5,6,7};
-    cout << s.maxIncreasingSubarrays(input2) << endl;
+    // vector<int> input = {2,5,7,8,9,2,3,4,3,1};
+    // vector<int> input2 = {1,2,3,4,4,4,4,5,6,7};
+    vector<int> input = {5,8,-2,-1};
+    cout << s.maxIncreasingSubarrays(input) << endl;
 }
